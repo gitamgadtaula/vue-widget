436822ff7a13f61b0b9aa7a6183ec9ca0753066b
gitam <gitam@rasello.com>
2021-03-15 13:25:19 +0545

    added vue-custom-plugin

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ title }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  props: { title: { default: "Send title" } },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
