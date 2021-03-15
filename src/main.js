5759511850ca8e6f52010cb30e0d5bb82d3bd048
gitam <gitam@rasello.com>
2021-03-15 14:27:03 +0545

    1st success demo

--- src/main.js
+++ src/main.js
@@ -9,4 +9,17 @@ Vue.use(customElement);
 // new Vue({
 //   render: (h) => h(App),
 // }).$mount("#app");
-Vue.customElement("vue-widget", App);
+// Vue.customElement("vue-widget", App);
+Vue.customElement('vue-widget', App, {
+    shadow: true,
+    beforeCreateVueInstance(root) {
+      const rootNode = root.el.getRootNode();
+  
+      if (rootNode instanceof ShadowRoot) {
+        root.shadowRoot = rootNode;
+      } else {
+        root.shadowRoot = document.head;
+      }
+      return root;
+    },
+  });
